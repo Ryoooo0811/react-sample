6fb52781174cc4b9f86be7f83ac1ed4402ecb736
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-10-17 22:32:17 +0900

    3.1.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';  // App.jsからApp関数を取り込んでいる。
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
+  // index.htmlにあるrootをIDにもつ要素を指定している。
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  // 描画するJSXタグを指定している。
   <React.StrictMode>
+    {/* Appはsrc/App.tsxからインポートしたものを使用している。 */}
     <App />
   </React.StrictMode>
 );
