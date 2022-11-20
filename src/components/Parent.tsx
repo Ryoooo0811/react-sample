722e38f4a7a69a948f45ab3578d9ebc88fab4adf
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-11-20 20:11:31 +0900

    onclickコールバック関数

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -13,39 +13,42 @@ const Fizz = (props: FizzProps) => {
   return <span>{isFizz ? 'Fizz' : ''}</span>
 }
 
+// type BuzzProps以降を編集
 type BuzzProps = {
   isBuzz: boolean
+  // propsにonClickを追加
+  onClick: () => void
 }
 
-// Buzzはメモ化した関数コンポーネント
-// isBuzzがtrueの場合はBuzzと表示し、それ以外は何も表示しない
-// 親コンポーネントが再描画されても、isBuzzが変化しない限りBuzzは再描画しない
 const Buzz = memo<BuzzProps>((props) => {
-  const {isBuzz} = props
+  const {isBuzz, onClick} = props
   console.log(`Buzzが再描画されました, isBuzz=${isBuzz}`)
   return (
-    <span>
+    <span onClick={onClick}>
       {isBuzz ? 'Buzz' : ''}
     </span>
   )
 })
 
-/* この形式でexportしたときは
-import {Parent} from ...
-で読み込む */
+
 export const Parent = () => {
   const [count, setCount] = useState(1)
   const isFizz = count % 3 === 0
   const isBuzz = count % 5 === 0
 
+  // この関数はParentの再描画の度に作成される
+  const onBuzzClick = () => {
+    console.log(`Buzzがクリックされました isBuzz=${isBuzz}`)
+  }
+
   console.log(`Parentが再描画されました, count=${count}`)
   return (
     <div>
-      <button onClick={() => setCount((c) => c+1)}>+1</button>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
       <p>{`現在のカウント: ${count}`}</p>
       <p>
         <Fizz isFizz={isBuzz} />
-        <Buzz isBuzz={isBuzz} />
+        <Buzz isBuzz={isBuzz} onClick={onBuzzClick} />
       </p>
     </div>
   )
